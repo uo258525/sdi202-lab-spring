fe5f5253e218a078af0c99bc3b3457f1d3af2be0
Elena <uo258525@uniovi.es>
2019-03-04 16:13:35 +0100

    SDI-Selenium-5.5-CasosUso-VistaRegister

--- src/test/java/com/uniovi/tests/NotaneitorTests.java
+++ src/test/java/com/uniovi/tests/NotaneitorTests.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import com.uniovi.tests.pageobjects.PO_HomeView;
 import com.uniovi.tests.pageobjects.PO_NavView;
 import com.uniovi.tests.pageobjects.PO_Properties;
+import com.uniovi.tests.pageobjects.PO_RegisterView;
+import com.uniovi.tests.pageobjects.PO_View;
 
 //Ordenamos las pruebas por el nombre del método
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -58,31 +60,67 @@ public class NotaneitorTests {
 		WebDriver driver = new FirefoxDriver();
 		return driver;
 	}
-	
-	//PR01. Acceder a la página principal /
+
+	// PR01. Acceder a la página principal /
 	@Test
 	public void PR01() {
 		PO_HomeView.checkWelcome(driver, PO_Properties.getSPANISH());
 	}
-	
-	//PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
+
+	// PR02. OPción de navegación. Pinchar en el enlace Registro en la página home
 	@Test
 	public void PR02() {
 		PO_NavView.clickOption(driver, "signup", "class", "btn btn-primary");
 	}
-	//PR03. OPción de navegación. Pinchar en el enlace Identificate en la página home
+
+	// PR03. OPción de navegación. Pinchar en el enlace Identificate en la página
+	// home
 	@Test
 	public void PR03() {
 		PO_NavView.clickOption(driver, "login", "class", "btn btn-primary");
 	}
-	
-	//PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a Español
+
+	// PR04. OPción de navegación. Cambio de idioma de Español a Ingles y vuelta a
+	// Español
 	@Test
 	public void PR04() {
-	PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish",
-	PO_Properties.getSPANISH(), PO_Properties.getENGLISH());
-	//SeleniumUtils.esperarSegundos(driver, 2);
+		PO_HomeView.checkChangeIdiom(driver, "btnSpanish", "btnEnglish", PO_Properties.getSPANISH(),
+				PO_Properties.getENGLISH());
+		// SeleniumUtils.esperarSegundos(driver, 2);
+	}
+
+	// PR05. Prueba del formulario de registro. registro con datos correctos
+	@Test
+	public void PR05() {
+		// Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		// Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "77777778A", "Josefo", "Perez", "77777", "77777");
+		// Comprobamos que entramos en la sección privada
+		PO_View.checkElement(driver, "text", "Notas del usuario");
 	}
 	
-	
+	//PR06. Prueba del formulario de registro. DNI repetido en la BD, Nombre corto, .... paginationpagination-centered, Error.signup.dni.length
+	@Test
+	public void PR06() {
+		//Vamos al formulario de registro
+		PO_HomeView.clickOption(driver, "signup", "class", "btn btn-primary");
+		//Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990A", "Josefo", "Perez", "77777",
+		"77777");
+		PO_View.getP();
+		//COmprobamos el error de DNI repetido.
+		PO_RegisterView.checkKey(driver, "Error.signup.dni.duplicate",
+		PO_Properties.getSPANISH() );
+		//Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Jose", "Perez", "77777",
+		"77777");
+		//COmprobamos el error de Nombre corto .
+		PO_RegisterView.checkKey(driver, "Error.signup.name.length",
+		PO_Properties.getSPANISH() );
+		//Rellenamos el formulario.
+		PO_RegisterView.fillForm(driver, "99999990B", "Josefo", "Per", "77777",
+		"77777");
+			}
+
 }
